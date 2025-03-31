8512b6a803278c57865c4db7481f0f261d71b06c
carrieyu3 <carrieyu03@gmail.com>
2025-03-30 21:08:09 -0400

    FAQ done

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
+
     var panel = this.nextElementSibling;
+
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
-    } else {
+    } 
+    else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
+    
   });
 }
